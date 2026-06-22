d71eba4473fdc5f88ae4c1acf684ec839ccdc379
ZetZor <98010233+IO-ZetZor@users.noreply.github.com>
2026-06-22 15:34:52 +0530

    Update windows_boot.h

--- include/windows_boot.h
+++ include/windows_boot.h
@@ -10,16 +10,10 @@ typedef struct {
     CHAR16 *description;
 } windows_boot_entry_t;
 
-EFI_STATUS windows_boot(boot_entry_t *entry, EFI_SYSTEM_TABLE *st);
-
 EFI_STATUS windows_find_bootmgr(CHAR16 *partition_uuid,
                                 EFI_DEVICE_PATH **bootmgr_path);
 
 EFI_DEVICE_PATH* windows_make_file_path(EFI_HANDLE handle,
                                         CHAR16 *filename);
-
-EFI_STATUS efi_chainload(EFI_HANDLE image_handle,
-                         EFI_SYSTEM_TABLE *system_table,
-                         CHAR16 *path);
 
 #endif
